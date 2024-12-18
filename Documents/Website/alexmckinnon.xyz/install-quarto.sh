38cd1a668be03b2f414072c3eb2dc6cef65c7240
Alexander Mckinnon <amckinnon@Alexanders-MacBook-Pro.local>
2024-12-18 17:16:52 +0000

    Update Quarto installation for Netlify build

--- Documents/Website/alexmckinnon.xyz/install-quarto.sh
+++ Documents/Website/alexmckinnon.xyz/install-quarto.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+# Download Quarto binary
 curl -fsSL https://quarto.org/download/latest/linux/x86_64/quarto-linux-amd64.tar.gz | tar -xz
-sudo cp -r quarto-linux-amd64/* /usr/local/
+
+# Install Quarto into Netlify's home directory
+mkdir -p /opt/buildhome/.local/bin
+cp -r quarto-linux-amd64/* /opt/buildhome/.local/bin/
+
+# Add Quarto to the PATH for the current session
+export PATH="/opt/buildhome/.local/bin:$PATH"
